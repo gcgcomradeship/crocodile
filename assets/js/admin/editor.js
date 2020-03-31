9a7f806089e2f7c8b30024f70640950ce980ce89
NS <kolud@bk.ru>
2020-03-31 14:37:58 +0300

    add ckeditor 4

--- assets/js/admin/editor.js
+++ assets/js/admin/editor.js
@@ -1,5 +1,6 @@
-ClassicEditor
-  .create( document.querySelector( '#editor' ) )
-  .catch( error => {
-      console.error( error );
-  } );
+// ClassicEditor
+//   .create( document.querySelector( '#editor' ) )
+//   .catch( error => {
+//       console.error( error );
+//   } );
+CKEDITOR.replace( 'editor' );
