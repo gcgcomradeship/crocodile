1cfa76b1243f3cdb34e9ce77c79b249eb07f1862
NS <kolud@bk.ru>
2020-06-01 16:34:26 +0300

    move ya metrics to special js file

---
+++ assets/static/js/metrics.js
@@ -0,0 +1,25 @@
+// Yandex.Metrika counter
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ym(62609776, "init", {
+  clickmap:true,
+  trackLinks:true,
+  accurateTrackBounce:true,
+  webvisor:true,
+  ecommerce:"dataLayer"
+});
+
+// // <!-- Global site tag (gtag.js) - Google Analytics -->
+// window.dataLayer = window.dataLayer |[];
+// function gtag(){dataLayer.push(arguments);}
+// gtag('js', new Date());
+// gtag('config', 'UA-165678985-1');
+
+
+// // Google Tag Manager
+// (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+// new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+// j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+// 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+// })(window,document,'script','dataLayer','GTM-TD6WH7S');
